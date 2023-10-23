ea397a2d1c023b06eaf9f676bc131f7230af1f58
Katja_EC <chermenina.ea@gmail.com>
2023-10-23 19:07:40 +0700

    Adds function for additional homework

--- js/functions.js
+++ js/functions.js
@@ -47,3 +47,33 @@ getNumbers('агент 007');
 getNumbers('а я томат');
 
 //console.log(getNumbers('а я томат'));
+
+
+const checkMeetingPossibility = (workingHoursStart, workingHoursEnd, meetingStart, meetingDuration) => {
+  const arrWorkingHoursStart = workingHoursStart.split(':');
+  const arrWorkingHoursEnd = workingHoursEnd.split(':');
+  const arrMeetingStart = meetingStart.split(':');
+  workingHoursStart = Number(arrWorkingHoursStart[0]) * 60 + Number(arrWorkingHoursStart[1]);
+  workingHoursEnd = Number(arrWorkingHoursEnd[0]) * 60 + Number(arrWorkingHoursEnd[1]);
+  meetingStart = Number(arrMeetingStart[0]) * 60 + Number(arrMeetingStart[1]);
+  const meetingHoursLeft = workingHoursEnd - meetingStart;
+  // console.log(workingHoursStart);
+  // console.log(workingHoursEnd);
+  // console.log(meetingStart);
+  // console.log(meetingHoursLeft);
+  // console.log(meetingStart >= workingHoursStart);
+  if (meetingHoursLeft === meetingDuration) {
+    return true;
+  } else if (meetingStart <= workingHoursStart || meetingStart >= workingHoursEnd || meetingHoursLeft <= meetingDuration) {
+    return false;
+  }
+  return meetingHoursLeft >= meetingDuration;
+
+};
+
+console.log(checkMeetingPossibility('8:15', '16:30', '15:00', 120));
+console.log(checkMeetingPossibility('08:00', '17:30', '14:00', 90)); // true
+console.log(checkMeetingPossibility('8:0', '10:0', '8:0', 120)); // true
+console.log(checkMeetingPossibility('08:00', '14:30', '14:00', 90)); // false
+console.log(checkMeetingPossibility('14:00', '17:30', '08:0', 90)); // false
+console.log(checkMeetingPossibility('8:00', '17:30', '08:00', 900)); // false
